931e4e0c3f723cd88c255ceac544a6152e11a2a0
Isuboyy <134434436+Isuboyy@users.noreply.github.com>
2024-11-03 16:30:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GiwGHTwI#B4QLEQc-5scp5sv0N4pTTE2NCbF41waPpTIjLwIj7Bs",
-MONGODB: process.env.MONGODB || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GiwGHTwI#B4QLEQc-5scp5sv0N4pTTE2NCbF41waPpTIjLwIj7Bs",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
